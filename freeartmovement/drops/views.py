eb551fa2c03c6dada709571b10360f5cf03e1f95
aljvapor <anthonyj@internationalvapor.com>
2017-10-18 19:10:43 -0400

    Created a working form that posts.

--- freeartmovement/drops/views.py
+++ freeartmovement/drops/views.py
@@ -1,11 +1,31 @@
-import uuid
+import uuid, datetime
+from django.contrib.auth.models import User
 from django.shortcuts import render
+
+from .models import ArtPiece
+from locations.models import City
 
 # Create your views here.
 def studio(request):
-    return render(request, 'studio.html',{})
+    artpieces = ArtPiece.objects.filter(creator=request.user)
+    return render(request, 'studio.html',{'artpeices':artpieces})
 
 def create(request):
     unique_drop_id = str(uuid.uuid4())
     trimmed_drop_id = unique_drop_id[:8]
-    return render(request, 'create.html',{'trimmed_uuid':trimmed_drop_id})
+    if request.method == 'POST':
+        title = request.POST['title']
+        confirmed_drop_id = request.POST['uuid']
+
+        user = User.objects.first()
+        city = City.objects.first()
+        drop = ArtPiece.objects.create(
+        creator = user,
+    	title = title,
+    	city = city,
+    	created_at = datetime.datetime.now(),
+    	url_slug = title,
+    	likes = 5
+        )
+        return render(request, 'created.html',{'uuid':unique_drop_id})
+    return render(request, 'create.html',{'uuid':unique_drop_id})
